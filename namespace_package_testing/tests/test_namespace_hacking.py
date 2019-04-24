80df92544433063404c2ee8dbb9d103daa03f383
Eric Berquist <berquist@isi.edu>
2019-04-24 18:03:44 -0400

    more namespace manipulation

--- namespace_package_testing/tests/test_namespace_hacking.py
+++ namespace_package_testing/tests/test_namespace_hacking.py
@@ -2,7 +2,16 @@ from namespace_package_testing.component_api_helper import package_hook
 
 
 def test_namespace_hacking():
+    # as you would normally do
     from namespace_package_testing.foo.bar.baz.meep import CONSTANT
-    from namespace_package_testing import meep
+
+    # this is from the top-level __init__.py
+    from namespace_package_testing import meep  # set_child_as_subpackage
+    import namespace_package_testing
+    print(namespace_package_testing.meep.CONSTANT)  # set_child_as_attr
+
+    import namespace_package_testing.baz  # is this the unfortunate side-effect?
+
+    # this is in-place
     package_hook("namespace_package_testing", "numpy.linalg")
     from namespace_package_testing.linalg import norm
